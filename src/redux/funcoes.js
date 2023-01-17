12f8dd1cf6d5ce16108662c3520d315fb38321ac
limasuu <szlima.93@gmail.com>
2023-01-17 10:06:53 -0300

    Criando a lógica dos botões de tipo numérico

--- src/redux/funcoes.js
+++ src/redux/funcoes.js
@@ -1,10 +1,34 @@
 export const funcaoClicarNumerico= (elemento, {atual, acumulado}) => {
     
-    let _atual= acumulado === '' ? '' : atual,
+    let _atual= atual,
         _acumulado= acumulado;
     
-    _atual+= elemento;
-    _acumulado+= elemento;
+    switch(elemento){
+        case '.':
+            if(!_atual.includes('.')){
+                _acumulado= (_acumulado === '') ? '0' + elemento : _acumulado + elemento;
+                _atual= (_atual === '') ? '0' + elemento : _atual + elemento;
+            }        
+            break;
+            
+        case '0': 
+            _atual= _acumulado === '' ? '' : _atual;
+            if(_atual !== '0'){
+                _atual+= elemento;
+                _acumulado+= elemento;
+            }
+            break;
+              
+        default:            
+            if(_atual === '0'){
+                _atual= elemento;
+                _acumulado= _acumulado.substring(0, _acumulado.length-1) + elemento;
+              
+            }else {       
+                _atual+= elemento;
+                _acumulado+= elemento;
+            }
+    }
     
     return {atual: _atual, acumulado: _acumulado};
 };
